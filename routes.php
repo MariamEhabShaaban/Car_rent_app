945e7a3cc67c97dce5d48fe63a58482051ba6e62
Mariam Ehab <99998896+MariamEhabShaaban@users.noreply.github.com>
2025-07-16 05:59:16 +0300

    Add files via upload

--- routes.php
+++ routes.php
@@ -15,7 +15,10 @@ $router->get('/add', 'cars/add.php')->only('Owner');
 $router->post('/store', 'cars/store.php')->only('Owner');
 $router->put('/update', 'cars/update.php')->only('Owner');
 $router->get('/login', 'welcome.php');
-
+$router->get('/accept', 'booking/response.php')->only('Owner');
+$router->get('/reject', 'booking/response.php')->only('Owner');
+$router->post('/return', 'booking/return.php')->only('Owner');
+$router->get('/no_answer', 'booking/no_answer.php')->only('Owner');
 // customer
 
 $router->get('/signup', 'customers/signup.php');
@@ -30,12 +33,17 @@ $router->post('/store_pass', 'customers/uploads/store_pass.php')->only('Customer
 $router->get('/upload_license', 'customers/uploads/upload_license.php')->only('Customer');
 $router->post('/store_license', 'customers/uploads/store_license.php')->only('Customer');
 $router->get('/time', 'customers/uploads/time.php')->only('Customer');
+
 $router->post('/store_time', 'customers/uploads/store_time.php')->only('Customer');
 $router->get('/payment_method', 'customers/uploads/payment_method.php')->only('Customer');
 $router->post('/store_pay', 'customers/uploads/store_pay.php')->only('Customer');
 
-$router->get('/send_email', 'customers/uploads/send_email.php')->only('Customer');
+$router->get('/send_email', 'customers/send_email.php')->only('Customer');
 
+$router->get('/rent_request','booking/requests.php')->only('Customer');
 
+$router->post('/store_rate', 'booking/store_rate.php')->only('Customer');
 
+$router->get('/rate', 'booking/rate.php')->only('Customer');
 
+$router->get('/ratings', 'booking/ratings.php')->only('Owner');
